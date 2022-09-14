3f84a14bbc1264c5b5d0b96e15f014daa3890b9e
camivand4 <camivand4@student.arteveldehs.be>
2022-09-14 02:08:10 +0200

    added wtb to admin panel

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -6,6 +6,7 @@ import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from 'firebase
 export const Admin = () => {
 
   const [newName, setNewName] = useState("");
+  const [newWTB, setNewWTB] = useState("");
 
   const usersCollectionRef = collection(db, 'users');
   const infoCollectionRef = collection(db, 'info');
@@ -41,50 +42,108 @@ export const Admin = () => {
     await addDoc(usersCollectionRef, {name: newName, active: false, paid: false})
   }
 
+  const createWTB = async () => {
+    await addDoc(wTBCollectionRef, {a: newWTB})
+  }
+
   const updateUser = async (id) => {
     const userDoc = doc(db, "users", id);
-    let newId = "name"+id;
-    let newName = document.getElementById(newId).value;
-    const newFields = {name: newName};
+
+    let nameId = "name"+id;
+    let newName = document.getElementById(nameId).value;
+
+    let activeId = "active"+id
+    let newActive = document.getElementById(activeId).checked;
+
+    let paidId = "paid"+id
+    let newPaid = document.getElementById(paidId).checked;
+
+    const newFields = {name: newName, active: newActive, paid: newPaid};
     await updateDoc(userDoc, newFields);
+  }
+
+  const updateWTB = async (id) => {
+    const wTBDoc = doc(db, "whatToBring", id);
+
+    let newValue = document.getElementById(id).value;
+
+    const newFields = {a: newValue};
+    await updateDoc(wTBDoc, newFields);
   }
 
   const deleteUser = async (id) => {
     const userDoc = doc(db, "users", id);
     await deleteDoc(userDoc);
   }
-  
+
+  const deleteWTB = async (id) => {
+    const wTBDoc = doc(db, "whatToBring", id);
+    await deleteDoc(wTBDoc);
+  }
+
   return (
     <div>
+      <h1>Users:</h1>
+
       <div className='margin-b-10'>
         <label htmlFor="name">Name: </label>
         <input type="text" id='name' onChange={(event) => { setNewName(event.target.value) }} />
         <button onClick={createUser}>Create</button>
       </div>
 
+      <div className='usersAdmin grid margin-b-10'>
+        {users.map((user, index) => (
+          <>
+          <div key={index+"name"+user.name} className="gridLine">
+            <label htmlFor="name">{user.name}: </label>
+            <input id={"name"+user.id} type="text" placeholder={user.name} defaultValue={user.name} />
+          </div>
+          <div key={index+"active"+user.name} className="gridLine">
+            <label htmlFor="active">Active: </label>
+            {user.active ? (
+              <input id={"active"+user.id} type="checkbox" defaultChecked value="true" />
+            ) : (
+              <input id={"active"+user.id} type="checkbox" value="false"/>
+            )}
+          </div>
+          <div key={index+"paid"+user.name} className="gridLine">
+            <label htmlFor="paid">Paid: </label>
+            {user.paid ? (
+              <input id={"paid"+user.id} type="checkbox" defaultChecked value="true" />
+            ) : (
+              <input id={"paid"+user.id} type="checkbox" value="false"/>
+            )}
+          </div>
+          <div key={index} className="gridLine">
+            <button className='updateButton' onClick={() => {updateUser(user.id)}}>Update</button>
+            <button className='deleteButton' onClick={() => {deleteUser(user.id)}}>Delete</button>
+          </div>
+          </>
+        ))}
+      </div>
 
-        <div className='usersAdmin grid'>
-          {users.map((user, index) => (
-            <>
-            <div key={index+"name"+user.name} className="gridLine">
-              <label htmlFor="name">{user.name}: </label>
-              <input id={"name"+user.id} type="text" placeholder={user.name} defaultValue={user.name} />
-            </div>
-            <div key={index+"active"+user.name} className="gridLine">
-              <label htmlFor="active">Active: </label>
-              <input id='active' type="checkbox" checked={user.active ? ('checked="checked"') : ("")} />
-            </div>
-            <div key={index+"paid"+user.name} className="gridLine">
-              <label htmlFor="paid">Paid: </label>
-              <input id='paid' type="checkbox" checked={user.paid ? ('checked="checked"') : ("")} />
-            </div>
-            <div key={index} className="gridLine">
-              <button className='updateButton' onClick={() => {updateUser(user.id)}}>Update</button>
-              <button className='deleteButton' onClick={() => {deleteUser(user.id)}}>Delete</button>
-            </div>
-            </>
-          ))}
-        </div>
+      <h1>Info:</h1>
+
+
+
+      <h1>What to bring:</h1>
+
+      <div className='margin-b-10'>
+        <label htmlFor="wTB">What to bring: </label>
+        <input type="text" id='wTB' onChange={(event) => { setNewWTB(event.target.value) }} />
+        <button onClick={createWTB}>Create</button>
+      </div>
+
+      <div className='wTBAdmin margin-b-10 flex column'>
+        {wTB.map((wtb, index) => (
+          <div className='flex'>
+            <input key={wtb.a+index} id={wtb.id} type="text" defaultValue={wtb.a} className="margin-b-10 wTBAdminInput"/>
+            <button className='updateButton' onClick={() => {updateWTB(wtb.id)}}>Update</button>
+            <button className='deleteButton' onClick={() => {deleteWTB(wtb.id)}}>Delete</button>
+          </div>
+        ))}
+      </div>
+
     </div>
   )
 }
